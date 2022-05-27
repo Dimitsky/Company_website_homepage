6ed66155f97e08ca446c22469c30829ed9ff7bfb
Evmenov Dmitry <dimitsky@icloud.com>
2022-05-27 23:34:01 +0400

    bug fix

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,12 +1,14 @@
 import gulp from 'gulp';
 import gulpPug from 'gulp-pug';
 import gulpHtmlMin from 'gulp-htmlmin';
+import gulpHtmlBeautify from 'gulp-html-beautify';
 import gulpIf from 'gulp-if';
 import { path } from '../config/path.js';
 
 export const pug = () => {
   return gulp.src( path.src.pug )
     .pipe( gulpPug( {} ) )
+    .pipe( gulpIf( app.isDev, gulpHtmlBeautify( { indentSize: 2 } ) ) )
     .pipe( gulpIf( app.isBuild, gulpHtmlMin( { collapseWhitespace: true } ) ) )
     .pipe( gulp.dest( path.dist.html ) )
 }
